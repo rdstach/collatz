50e0fe2b153b684d39c6b5e8461ea25947f55372
Roger Stach <rogerdanistach@gmail.com>
2018-01-05 13:16:03 +0700

    SRC Directory

---
+++ src/collatz.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const print = console.log
+const argv = process.argv
+
+function algo(num) {
+	let totalNum = []
+	totalNum.push(num)
+	print(num)
+
+	while (num != 1) {
+		if (num % 2 == 0) {
+			num = num / 2
+			totalNum.push(num)
+			print(num)
+		} else {
+			num = 3 * num + 1
+			totalNum.push(num)
+			print(num)
+		}
+	}
+
+	print("\nTotal Number : " + totalNum.length)
+	return process.exit()
+}
+
+if (argv.length != 3) {
+	print("The program needs an argument in order to run.\nPlease read the README.")
+} else {
+	let result = algo(parseInt(argv[2]))
+	print(result)
+}
