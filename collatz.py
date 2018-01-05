ca0af196fc939ec0fb08bcec363ef3f30e285796
Roger Stach <rogerdanistach@gmail.com>
2018-01-05 12:53:50 +0700

    Print the process length in Python

--- collatz.py
+++ collatz.py
@@ -3,15 +3,20 @@
 import sys
 
 def algo(num):
+        totalNum = []
 	print(num)
 
 	while num != 1:
 		if num % 2 == 0:
 			num = num / 2
+                        totalNum.append(num)
 			print(num)
 		else:
 			num = 3 * num + 1
+                        totalNum.append(num)
 			print(num)
+
+        print "\nTotal Number :", len(totalNum)
 
 def main():
 	argv = int(sys.argv[1])
